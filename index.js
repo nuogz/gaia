4bdbc756eb920d1b516c20b247ccc435e947a4f5
DanoR <zheung@vip.qq.com>
2021-05-06 17:14:16 +0800

    remove `configMan.js` and `log.js`; unify various configuration files; update dependencies

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
 	// classes or object-like functions
-	log: require('./log'),
-	configMan: require('./configMan'),
 
 	// standalone funcitons
 	getSafe: require('func/getSafe'),
