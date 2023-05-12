1ba336b72c62e2b3fe2a13f917612db1e74052bb
Nuogz <zheung@vip.qq.com>
2023-05-12 16:45:36 +0800

    * preset: move `index.env.js` to preset `node`

--- preset/node/index.env.js
+++ preset/node/index.env.js
@@ -1,3 +1,6 @@
 process.env.NENV_PKG_PATH = import.meta.url;
+
+process.env.NENV_I18N_LOCALE = 'zh';
+process.env.NENV_I18N_FORMAT = 'hades';
+
 process.env.NENV_PANGU = '*';
-process.env.OUTPUT_FORMAT = 'hades';
